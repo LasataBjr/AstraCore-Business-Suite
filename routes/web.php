fd42a7f16455a5f43c1b644c1d69d6c7cb94dcf3
Lasata Bajracharya <lasatabjrktm@gmail.com>
2026-06-03 17:04:15 +0545

    fix: changed admin route name

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\AdminDashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,17 +9,19 @@ Route::get('/', function () {
 });
 
 // Protected routes exclusively for Admins
-Route::middleware(['auth', 'admin']) 
+Route::middleware(['auth', 'admin']) // Applying both authentication and admin middleware
     ->prefix('admin') // URL prefix for admin routes
     ->name('admin.') // Route name prefix for admin routes
     ->group(function () { // Grouping admin routes together
-    Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
+    Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
 });
 
+// Protected routes for authenticated users (both Admins and regular users)
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Profile management routes for authenticated users by breeze default
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
